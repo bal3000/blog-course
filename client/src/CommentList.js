3081d8bf2b161726d87b06ffc9f93358d76e8857
bal3000 <bal3000286@googlemail.com>
2020-06-30 14:36:01 +0100

    first commit

---
+++ client/src/CommentList.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
+    if (comment.status === 'approved') {
+      content = comment.content;
+    } else if (comment.status === 'rejected') {
+      content = 'Comment Rejected';
+    } else {
+      content = 'Awaiting Moderation';
+    }
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
---
+++ comments/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  const commentToCreate = { id: commentId, content, status: 'pending' };
+  comments.push(commentToCreate);
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      ...commentToCreate,
+      postId: req.params.id,
+    },
+  });
+
+  res.status(201).send(comments);
+});
+
+app.post('/events', async (req, res) => {
+  console.log(`Received Event ${req.body.type}`);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { id, postId, status } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((c) => c.id === id);
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        ...comment,
+        postId,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
